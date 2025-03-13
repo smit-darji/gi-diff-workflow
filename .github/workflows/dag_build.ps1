ab4990912e74c44717207a5f0792ad85e58f0ca2
smit-darji <smit.softvan@gmail3.com>
2025-03-13 18:05:08 +0530

    added latest code

--- .github/workflows/dag_build.ps1
+++ .github/workflows/dag_build.ps1
@@ -9,14 +9,4 @@ Write-Host "Creating folder structure..."
 mkdir '.\temp\dna-datalake-airflow\dag' -Force
 mkdir '.\temp\dna-datalake-airflow\config' -Force
 
-# Read added/modified files from environment variable
-if ($env:ADDED_FILES -ne "[]") {
-    Write-Host "`nAdded/Modified Files:"
-    $env:ADDED_FILES | ConvertFrom-Json | ForEach-Object { Write-Host $_ }
-} else {
-    Write-Host "No added/modified files found."
-}
-
-# Print created folder structure (for debugging)
-Write-Host "`n=== Folder Structure ==="
-Get-ChildItem -Recurse .\temp
+Write-Host "ADDED_FILES content: $env:ADDED_FILES"
