a954ab9d11e19272c6f39c9ecf1f41549e50be0c
smit-darji <smit.softvan@gmail3.com>
2025-03-13 18:31:20 +0530

    added latest code

--- .github/workflows/dag_build.ps1
+++ .github/workflows/dag_build.ps1
@@ -2,5 +2,15 @@
 $addedFiles = $env:ADDED_FILES
 
 # Print the added files to the console
-Write-Host "Added Files:"
+Write-Host "Changed Files:"
 Write-Host $addedFiles
+
+$FolderName = ".\temp"
+if (Test-Path $FolderName) {
+    Write-Host "Folder Exists"
+    Remove-Item $FolderName  -Recurse -Force
+}
+mkdir '.\temp\dna-datalake-airflow\dag'
+mkdir '.\temp\dna-datalake-airflow\config'
+Copy-Item -Path ".\DAG\*" -Destination ".\temp\dna-datalake-airflow\dag" -Recurse
+Copy-Item -Path ".\config\*" -Destination ".\temp\dna-datalake-airflow\config" -Recurse
