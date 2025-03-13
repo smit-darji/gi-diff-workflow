e1118dba28f279c81d9e879607688890c82ddbe4
smit-darji <smit.softvan@gmail3.com>
2025-03-13 18:38:37 +0530

    added latest code

--- .github/workflows/dag_build.ps1
+++ .github/workflows/dag_build.ps1
@@ -1,16 +1,25 @@
-# Read the ADDED_FILES environment variable from GitHub
-$addedFiles = $env:ADDED_FILES
+# # Read the ADDED_FILES environment variable from GitHub
+# $addedFiles = $env:ADDED_FILES
 
-# Print the added files to the console
-Write-Host "Changed Files:"
-Write-Host $addedFiles
+# # Print the added files to the console
+# Write-Host "Changed Files:"
+# Write-Host $addedFiles
 
-$FolderName = ".\temp"
-if (Test-Path $FolderName) {
-    Write-Host "Folder Exists"
-    Remove-Item $FolderName  -Recurse -Force
+# $FolderName = ".\temp"
+# if (Test-Path $FolderName) {
+#     Write-Host "Folder Exists"
+#     Remove-Item $FolderName  -Recurse -Force
+# }
+# mkdir '.\temp\dna-datalake-airflow\dag'
+# mkdir '.\temp\dna-datalake-airflow\config'
+# Copy-Item -Path ".\DAG\*" -Destination ".\temp\dna-datalake-airflow\dag" -Recurse
+# Copy-Item -Path ".\config\*" -Destination ".\temp\dna-datalake-airflow\config" -Recurse
+
+# Read FILE_MODIFIED from GitHub environment
+$fileModified = $env:FILE_MODIFIED
+
+if ($fileModified -eq 'true') {
+    Write-Host "config/requirements_2.7.2.txt is modified"
+} else {
+    Write-Host "config/requirements_2.7.2.txt is not modified"
 }
-mkdir '.\temp\dna-datalake-airflow\dag'
-mkdir '.\temp\dna-datalake-airflow\config'
-Copy-Item -Path ".\DAG\*" -Destination ".\temp\dna-datalake-airflow\dag" -Recurse
-Copy-Item -Path ".\config\*" -Destination ".\temp\dna-datalake-airflow\config" -Recurse
