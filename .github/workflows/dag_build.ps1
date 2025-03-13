e5e11c0ddb8a2dfdf9ec8d909e31486e4875397e
smit-darji <smit.softvan@gmail3.com>
2025-03-13 18:41:18 +0530

    added latest code

--- .github/workflows/dag_build.ps1
+++ .github/workflows/dag_build.ps1
@@ -1,20 +1,3 @@
-# # Read the ADDED_FILES environment variable from GitHub
-# $addedFiles = $env:ADDED_FILES
-
-# # Print the added files to the console
-# Write-Host "Changed Files:"
-# Write-Host $addedFiles
-
-# $FolderName = ".\temp"
-# if (Test-Path $FolderName) {
-#     Write-Host "Folder Exists"
-#     Remove-Item $FolderName  -Recurse -Force
-# }
-# mkdir '.\temp\dna-datalake-airflow\dag'
-# mkdir '.\temp\dna-datalake-airflow\config'
-# Copy-Item -Path ".\DAG\*" -Destination ".\temp\dna-datalake-airflow\dag" -Recurse
-# Copy-Item -Path ".\config\*" -Destination ".\temp\dna-datalake-airflow\config" -Recurse
-
 # Read FILE_MODIFIED from GitHub environment
 $fileModified = $env:FILE_MODIFIED
 
@@ -23,3 +6,30 @@ if ($fileModified -eq 'true') {
 } else {
     Write-Host "config/requirements_2.7.2.txt is not modified"
 }
+
+$FolderName = ".\temp"
+if (Test-Path $FolderName) {
+    Write-Host "Folder Exists"
+    Remove-Item $FolderName -Recurse -Force
+}
+
+# Create required directories
+mkdir '.\temp\dna-datalake-airflow\dag'
+mkdir '.\temp\dna-datalake-airflow\config'
+
+# Copy DAG files to destination
+Copy-Item -Path ".\DAG\*" -Destination ".\temp\dna-datalake-airflow\dag" -Recurse
+
+# Get list of all files in config folder
+$configFiles = Get-ChildItem -Path ".\config" -File
+
+foreach ($file in $configFiles) {
+    if ($file.Name -eq 'requirements_2.7.2.txt' -and $fileModified -ne 'true') {
+        Write-Host "Skipping unmodified file: $($file.Name)"
+        continue
+    }
+    Write-Host "Copying file: $($file.Name)"
+    Copy-Item -Path $file.FullName -Destination ".\temp\dna-datalake-airflow\config"
+}
+
+Write-Host "Build completed"
